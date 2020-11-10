a8e555a36c24e4a703c545dd3fbe208fc72ffccb
Eric Raslich <rasliche@gmail.com>
2020-11-10 15:24:05 -0500

    get a form ID

--- src/site/_data/results.js
+++ src/site/_data/results.js
@@ -4,8 +4,9 @@ const NetlifyAPI = require('netlify')
 const client = new NetlifyAPI(netlifyPersonalAccessToken())
 
 module.exports = async function() {
-    const results = await client.listSiteSubmissions({
-        form_id: 'results',
+    const results = await client.listSiteForms({
+    // const results = await client.listFormSubmissions({
+        form_id: '5faabd3e58099c00076a057d',
         site_id: kwgp5kSiteId(),
     })
     // return results.json()
