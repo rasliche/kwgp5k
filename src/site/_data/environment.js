2b52815a56583c663edca0858bea6e60b93260cd
Eric Raslich <rasliche@gmail.com>
2020-11-10 10:38:25 -0500

    Return Stripe API secrets

--- src/site/_data/environment.js
+++ src/site/_data/environment.js
@@ -6,6 +6,13 @@ module.exports = {
             return process.env.STRIPE_TEST_PUBLISHABLE_KEY
         }
     },
+    stripeApiSecret: () => {
+        if (process.env.CONTEXT === 'production') {
+            return process.env.STRIPE_PROD_API_SECRET
+        } else {
+            return process.env.STRIPE_TEST_API_SECRET
+        }
+    },
     netlifyPersonalAccessToken: () => {
         // Netlify User dashboard as kwgp5k_race_results
         return process.env.NETLIFY_PERSONAL_ACCESS_TOKEN
