9cd57379b72f285bce86398cfbe1b83a03a7622c
Eric Raslich <rasliche@gmail.com>
2020-11-11 22:24:34 -0500

    hidden results

--- src/site/_data/results.js
+++ src/site/_data/results.js
@@ -5,9 +5,14 @@ const client = new NetlifyAPI(netlifyPersonalAccessToken())
 
 module.exports = async function() {
     // const results = await client.listSiteForms({
-    const results = await client.listFormSubmissions({
+    const response = await client.listFormSubmissions({
         form_id: '5faaf705b3d68600072ab8dc',
         site_id: kwgp5kSiteId(),
+    })
+
+    const results = response.map(r => {
+        const { name, email, distance, time, fuel, location, spirit, ...rest } = r.data
+        return { name, email, distance, time, fuel, location, spirit, }
     })
     // return results.json()
     // const racerEmails = results.map(r => r.email)
